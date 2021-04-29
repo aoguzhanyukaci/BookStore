19c1c9b14c1c635b2d4855dd1ec7a0af66df46b0
Oğuzhan Yukacı <aoguzhanyukaci@gmail.com>
2021-04-29 13:06:47 +0300

    Seed

---
+++ src/ApplicationCore/Constans/AuthorizationConstants.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Constans
+{
+    public class AuthorizationConstants
+    {
+        public const string DEFAULT_PASSWORD = "Ankara1.";
+
+        public static class Roles
+        {
+            public const string ADMIN = "admin";
+        }
+    }
+}
