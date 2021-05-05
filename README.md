e9dceeb5d70bc7337718f85646a0f4ebc8ba0bde
Oğuzhan Yukacı <aoguzhanyukaci@gmail.com>
2021-05-05 10:48:36 +0300

    Order Basket Migrations

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
 First set src/Infrastructure as default project nin package manager console
+
 Add-Migration InitialApp -OutputDir Data/Migrations -Context Infrastructure.Data.AppDbContext -StartupProject Web
+
 Update-Database -Context Infrastructure.Data.AppDbContext
 
 
 Add-Migration InitialIdentity -OutputDir Identity/Migrations -Context Infrastructure.Identity.AppIdentityDbContext -StartupProject Web
 Update-Database -Context Infrastructure.Identity.AppIdentityDbContext
+
+Add-Migration OrderBasketAded -OutputDir Data/Migrations -Context Infrastructure.Data.AppDbContext -StartupProject Web
+update-database -context AppDbContext
+
+
+remove-migration -context AppDbContext
---
+++ src/Infrastructure/Data/Config/OrderItemsConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using ApplicationCore.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data.Config
+{
+    public class OrderItemsConfiguration : IEntityTypeConfiguration<OrderItem>
+    {
+        public void Configure(EntityTypeBuilder<OrderItem> builder)
+        {
+
+            builder.Property(b => b.ProductName)
+                .IsRequired()
+                .HasMaxLength(18);
+
+            builder.Property(b => b.Price)
+                .IsRequired()
+                .HasColumnType("decimal(18,2)");
+
+        }
+    }
+}
